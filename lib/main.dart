b38ba4531d2968a12cbb4c6d5453585941dfdfbf
Frendy Hariyono <c14170108@john.petra.ac.id>
2022-07-02 07:46:49 +0700

    UI Project

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,24 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Profile_Screen();
+            } else {
+              return HomePage();
+            }
+          },
+        ),
+      );
+}
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -39,7 +57,7 @@ class _HomePageState extends State<HomePage> {
           future: _initializeFirebase(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
-              return LoginScreen();
+              return MainPage();
             }
             return const Center(
               child: CircularProgressIndicator(),
@@ -168,8 +186,160 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
               ),
             ),
-          )
+          ),
+          Container(
+            width: double.infinity,
+            child: RawMaterialButton(
+              fillColor: Colors.transparent,
+              elevation: 0.0,
+              padding: EdgeInsets.symmetric(vertical: 1.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              onPressed: () async {
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => Register_Page()));
+              },
+              child: Text(
+                "Don't have account? Register",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15.0,
+                ),
+              ),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+//Register Page
+class Register_Page extends StatefulWidget {
+  const Register_Page({Key? key}) : super(key: key);
+
+  @override
+  State<Register_Page> createState() => _Register_PageState();
+}
+
+//page Register
+class _Register_PageState extends State<Register_Page> {
+  static Future<User?> loginUsingEmailPassword(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        print("No User found or no email");
+      }
+    }
+    return user;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+    FirebaseAuth auth = FirebaseAuth.instance;
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "MyApp Title",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "login to your App",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 44.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 44.0,
+            ),
+            TextField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: "User Email",
+                prefixIcon: Icon(
+                  Icons.mail,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 26.0,
+            ),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "User password",
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 12.0,
+            ),
+            Text(
+              "Forget your Password?",
+              style: TextStyle(color: Colors.blue),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Container(
+              width: double.infinity,
+              child: RawMaterialButton(
+                fillColor: Colors.blue,
+                elevation: 0.0,
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                onPressed: () async {
+                  auth.createUserWithEmailAndPassword(
+                      email: _emailController.text,
+                      password: _passwordController.text);
+                  // User? user = await loginUsingEmailPassword(
+                  //     email: _emailController.text,
+                  //     password: _passwordController.text,
+                  //     context: context);
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => HomePage()));
+                },
+                child: Text(
+                  "SIGN UP  ",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
